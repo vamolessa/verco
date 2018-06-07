87eb6d21fc105219d1128f4a9593556dc612afc9
Matheus Lessa <matheuslessarodrigues@gmail.com>
2018-06-07 17:03:14 -0300

    quase funciona abrir o explorer no path certo

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,35 @@ extern crate termion;
 use std::env;
 use std::path::PathBuf;
 
-mod tui;
-mod version_control_actions;
 mod git_actions;
 mod hg_actions;
+mod tui;
+mod version_control_actions;
 
 use git_actions::GitActions;
 use hg_actions::HgActions;
 
 fn main() {
-	let current_dir_path = env::current_dir().unwrap();
-	let current_dir = current_dir_path.to_str().unwrap();
+    let current_dir_path = env::current_dir().unwrap();
+    let current_dir = current_dir_path.to_str().unwrap();
 
-	if subdir_exists(&current_dir_path, ".git") {
-		let actions = GitActions {
-			current_dir: &current_dir,
-		};
-		tui::show_tui(&current_dir, &actions);
-	} else if subdir_exists(&current_dir_path, ".hg") {
-		let actions = HgActions {
-			current_dir: &current_dir,
-		};
-		tui::show_tui(&current_dir, &actions);
-	} else {
-		println!("no repository found");
-	}
+    if subdir_exists(&current_dir_path, ".git") {
+        let actions = GitActions {
+            current_dir: &current_dir,
+        };
+        tui::show_tui(&current_dir, &actions);
+    } else if subdir_exists(&current_dir_path, ".hg") {
+        let actions = HgActions {
+            current_dir: &current_dir,
+        };
+        tui::show_tui(&current_dir, &actions);
+    } else {
+        println!("no repository found");
+    }
 }
 
 fn subdir_exists(basedir: &PathBuf, subdir: &str) -> bool {
-	let mut path = basedir.clone();
-	path.push(subdir);
-	path.exists()
+    let mut path = basedir.clone();
+    path.push(subdir);
+    path.exists()
 }
