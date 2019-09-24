ec9ba71155d413fd2ca6e8a7a977e61bddae5c2d
Matheus Lessa <matheus@fastmail.com>
2019-09-23 21:19:01 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,10 @@ mod version_control_actions;
 fn main() {
 	ctrlc::set_handler(move || {}).unwrap();
 
-	//let version_controls = repositories::get_version_controls().unwrap();
-	//repositories::set_version_controls(&version_controls).unwrap();
-	let version_controls = repositories::get_current_version_control().unwrap();
-
-	if version_controls.len() == 0 {
-		println!("no repository found");
+	if let Some(version_control) = repositories::get_current_version_control() {
+		tui::show_tui(vec![version_control]);
 	} else {
-		tui::show_tui(version_controls);
+		eprintln!("no repository found");
 	}
 }
+//
