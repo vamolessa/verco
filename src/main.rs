dd1bf449d422f81673f205add26c705e4164c7a5
Matheus Lessa <matheus@fastmail.com>
2020-03-26 11:49:46 -0300

    remove cli options. keep it simple

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-mod cli;
 mod ctrlc_handler;
 mod custom_commands;
 mod git_actions;
@@ -13,23 +12,17 @@ mod tui_util;
 mod version_control_actions;
 
 fn main() {
-    match cli::handle_cli_options() {
-        Ok(true) => {
-            let handler = ctrlc_handler::CtrlcHandler::new();
-            let handler_clone = handler.clone();
-            ctrlc::set_handler(move || {
-                handler_clone.set(true);
-            })
-            .unwrap();
+    let handler = ctrlc_handler::CtrlcHandler::new();
+    let handler_clone = handler.clone();
+    ctrlc::set_handler(move || {
+        handler_clone.set(true);
+    })
+    .unwrap();
 
-            if let Some(version_control) = repositories::get_current_version_control() {
-                let custom_commands = custom_commands::CustomCommand::load_custom_commands();
-                tui::show_tui(version_control, custom_commands, handler);
-            } else {
-                eprintln!("no repository found");
-            }
-        }
-        Ok(false) => (),
-        Err(error) => eprintln!("{}", error),
+    if let Some(version_control) = repositories::get_current_version_control() {
+        let custom_commands = custom_commands::CustomCommand::load_custom_commands();
+        tui::show_tui(version_control, custom_commands, handler);
+    } else {
+        eprintln!("no repository found");
     }
 }
