6b631955b85e7dd88352c968a083b5fc50291a0b
Matheus Lessa <matheus@fastmail.com>
2020-04-17 15:15:14 -0300

    now verco can receive a new starting directory as an argument

--- src/repositories.rs
+++ src/repositories.rs
@@ -1,19 +1,27 @@
-use std::{env, path::PathBuf};
+use std::{env, path::{Path, PathBuf}};
 
 use crate::git_actions::GitActions;
 use crate::hg_actions::HgActions;
 use crate::version_control_actions::VersionControlActions;
 
 pub fn get_current_version_control() -> Option<Box<dyn VersionControlActions>> {
-    let current_dir = env::current_dir().unwrap();
+    let mut args = env::args();
+    if let Some(dir) = args.nth(1) {
+        let dir = Path::new(&dir);
+        dir.canonicalize().expect("could not canonicalize path");
+        env::set_current_dir(dir).expect("could not set current directory");
+    }
+
+    let current_dir = env::current_dir().expect("could not get current directory");
+
     if subdir_exists(&current_dir, ".git") {
         Some(Box::from(GitActions {
-            current_dir: current_dir.to_str().unwrap().into(),
+            current_dir: current_dir.to_str().expect("current directory is not valid utf8").into(),
             revision_shortcut: Default::default(),
         }))
     } else if subdir_exists(&current_dir, ".hg") {
         Some(Box::from(HgActions {
-            current_dir: current_dir.to_str().unwrap().into(),
+            current_dir: current_dir.to_str().expect("current directory is not valid utf8").into(),
             revision_shortcut: Default::default(),
         }))
     } else {
